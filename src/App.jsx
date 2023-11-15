a5dc2d736654fff7be16a363edbbbf115c397f7b
umarfaruk2 <farukdev22@gmail.com>
2023-11-16 00:07:57 +0600

    food delivery

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  
+  return (
+    <div className="bg-blue-500">
+      <p>hello</p> 
+    </div> 
+  )
+}
+
+export default App
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h3 className='bg-gray-500'>This is home page</h3>
+        </div>
+    );
+};
+
+export default Home;
